0c30b7274ac697bc5db8a3a68208ca9b9bce7f68
Shariful Islam <ether_sb@yahoo.com>
2023-07-07 19:46:57 +0600

    responsive

--- public/assets/resource/js/apps.js
+++ public/assets/resource/js/apps.js
@@ -27,7 +27,7 @@ function leftNavC() {
 
 // scroll nav
 window.onscroll = function () {
-  if (window.pageYOffset >= nav.offsetTop) {
+  if (window.pageYOffset >= 300) {
     navLogo.style.display = "block";
   } else {
     navLogo.style.display = "none";
